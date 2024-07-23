2aff59cd8955b6905c3ec1566f72fa95615860a6
heonuk <gitshineit@gmail.com>
2024-07-23 23:37:29 +0900

    :heavy_plus_sign: Set: Lombok 라이브러리 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.checkmate {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires static lombok;
 
     opens org.checkmate to javafx.fxml;
     exports org.checkmate;
