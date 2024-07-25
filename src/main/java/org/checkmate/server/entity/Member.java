08e89b1524e3ddbe629636d31d889dd66b177be9
heonuk <gitshineit@gmail.com>
2024-07-25 12:11:35 +0900

    :wrench: fix: Lombok 적용 및 사원 이름 컬럼 추가

--- src/main/java/org/checkmate/server/entity/Member.java
+++ src/main/java/org/checkmate/server/entity/Member.java
@@ -1,30 +1,36 @@
 package org.checkmate.server.entity;
 
 import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * 서비스 이용자 객체
  * HISTORY1: 최초 생성                              [송헌욱  2024.07.24]
  * HISTORY2: ROLE get메서드 추가                     [이준희  2024.07.24]
+ * HISTORY3: Lombok 적용 및 사원 이름 컬럼 추가         [송헌욱  2024.07.25]
  */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
-    private final long memberId;    // 고유 식별자
-    private final String loginId;   // 로그인ID(사원 번호)
-    private final String password;  // 비밀번호
-    private final MRole role;       // 권한 (예: ADMIN - 관리자, BASIC - 일반 사원)
-    private final int delayCnt;     // 도서 연체 횟수
+    private long memberId;    // 고유 식별자
+    private String loginId;   // 로그인ID(사원 번호)
+    private String password;  // 비밀번호
+    private String eName;      // 사원 이름
+    private MRole role;       // 권한 (예: ADMIN - 관리자, BASIC - 일반 사원)
+    private int delayCnt;     // 도서 연체 횟수
 
-    public Member(long memberId, String loginId, String password, MRole role, int delayCnt) {
+    @Builder
+    public Member(long memberId, String loginId, String password, String eName, MRole role, int delayCnt) {
         this.memberId = memberId;
         this.loginId = loginId;
         this.password = password;
+        this.eName = eName;
         this.role = role;
         this.delayCnt = delayCnt;
-    }
-
-    public MRole getRole() {
-        return role;
     }
 
     @Override
@@ -44,17 +50,6 @@ public class Member {
     @Override
     public int hashCode() {
         return Objects.hash(memberId, loginId, password, role, delayCnt);
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "memberId=" + memberId +
-                ", loginId='" + loginId + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                ", delayCnt=" + delayCnt +
-                '}';
     }
 
 }
