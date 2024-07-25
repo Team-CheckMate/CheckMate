69c985431a3e44f69eb285cef61f953761413e4e
JUNI <junehee107@naver.com>
2024-07-25 18:38:23 +0900

    :sparkles: feat : 패스워드 update 추상 메서드 추가

--- src/main/java/org/checkmate/server/service/MemberService.java
+++ src/main/java/org/checkmate/server/service/MemberService.java
@@ -1,15 +1,18 @@
 package org.checkmate.server.service;
 
+import org.checkmate.server.dto.request.ChangePwRequestDto;
 import java.sql.SQLException;
 import org.checkmate.server.dto.request.LoginRequestDto;
+import org.checkmate.server.dto.response.ChangePwResponseDto;
 import org.checkmate.server.dto.response.MemberInfoResponseDto;
 
 /**
  * 회원 서비스 인터페이스
  * HISTORY1: 최초 생성                              [송헌욱  2024.07.24]
+ * HISTORY2: pw 업데이트 추가                        [이준희  2024.07.25]
  */
 public interface MemberService {
 
     MemberInfoResponseDto login(LoginRequestDto requestDto) throws SQLException;
-
+    ChangePwResponseDto changePw(ChangePwRequestDto changePwRequestDto);
 }
