553b07b09fbc3962eda76f925cb2b5a742c1cd06
JUNI <junehee107@naver.com>
2024-07-25 18:35:45 +0900

    :sparkles: feat : 패스워드 update 매핑 메서드 추가

--- src/main/java/org/checkmate/server/mapper/MemberMapper.java
+++ src/main/java/org/checkmate/server/mapper/MemberMapper.java
@@ -17,6 +17,7 @@ import org.checkmate.server.entity.Member;
  * SQL Query mapper 클래스
  * HISTORY1: 최초 생성                              [송헌욱  2024.07.24]
  * HISTORY2: Optional 타입 선언                     [송헌욱  2024.07.25]
+ * HISTORY3: 패스워드 Update 매핑 추가                [이준희  2024.07.25]
  */
 public class MemberMapper {
 
@@ -59,6 +60,23 @@ public class MemberMapper {
             }
         }
         return Optional.empty();
+    }
+
+    /**
+     * SQL에 접근하여 이용자의 패스워드를 입력받은 값으로 UPDATE 하는 기능
+     * @param memberId 로그인 pk
+     * @param changePassword 새로운 비밀 번호
+     * @return updateRows 변경된 쿼리 개수
+     * @throws SQLException SQL 서버 에러
+     */
+    public int updateMemberPassword  (long memberId, String changePassword) throws SQLException {
+        String query = prop.getProperty("updateMemberPassword");
+        try (Connection connection = DBConnector.getInstance().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setLong(1, memberId);
+            preparedStatement.setString(2, changePassword);
+            return preparedStatement.executeUpdate();
+        }
     }
 
 }
