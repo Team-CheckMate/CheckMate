05990c03ab84949cd6fda50766642066e46f845a
heonuk <gitshineit@gmail.com>
2024-07-25 12:18:47 +0900

    :sparkles: feat: 모듈 권한 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,11 @@ module org.checkmate {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires static lombok;
 
     opens org.checkmate to javafx.fxml;
     exports org.checkmate;
     exports org.checkmate.server.controller;
     opens org.checkmate.server.controller to javafx.fxml;
+    opens org.checkmate.server.entity;
 }
