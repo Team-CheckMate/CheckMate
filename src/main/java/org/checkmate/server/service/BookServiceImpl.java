c6c4d584c0d920febe25e102a4e9204e4d4903d5
JUNI <junehee107@naver.com>
2024-07-26 18:28:13 +0900

    :sparkles: feat : 관리자 도서 등록 구현 메서드 추가

--- src/main/java/org/checkmate/server/service/BookServiceImpl.java
+++ src/main/java/org/checkmate/server/service/BookServiceImpl.java
@@ -2,12 +2,16 @@ package org.checkmate.server.service;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import org.checkmate.server.dto.request.AddBookRequestDto;
+import org.checkmate.server.dto.response.AddBookResponseDto;
 import org.checkmate.server.entity.BookLoanStatus;
 import org.checkmate.server.mapper.BookMapper;
 
 /**
  * 도서 서비스 구현 클래스
  * HISTORY1: 최초 생성                              [송헌욱  2024.07.24]
+ * HISTORY2: 도서 등록 기능 추가                      [이준희  2024.07.26]
  */
 public class BookServiceImpl implements BookService {
 
@@ -25,5 +29,15 @@ public class BookServiceImpl implements BookService {
     @Override
     public List<BookLoanStatus> findAllBooks() throws SQLException {
         return bookMapper.findAllBookLoanStatus();
+    }
+
+    /**
+     * 관리자가 도서를 추가하는 기능
+     * @return AddBookResponseDto 등록된 도서 정보 결과
+     * @throws SQLException DataBase 에러
+     */
+    @Override
+    public AddBookResponseDto addBook(AddBookRequestDto requestDto) throws SQLException {
+        return bookMapper.addNewBook(requestDto);
     }
 }
