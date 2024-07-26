6f2b449a09e64e9b16ae4b07e8571ea5cdddaf59
JUNI <junehee107@naver.com>
2024-07-26 18:27:06 +0900

    :sparkles: feat : 관리자 도서 등록 추상 메서드 추가

--- src/main/java/org/checkmate/server/service/BookService.java
+++ src/main/java/org/checkmate/server/service/BookService.java
@@ -2,10 +2,15 @@ package org.checkmate.server.service;
 
 import java.sql.SQLException;
 import java.util.List;
+
+import org.checkmate.server.dto.request.AddBookRequestDto;
+import org.checkmate.server.dto.response.AddBookResponseDto;
 import org.checkmate.server.entity.BookLoanStatus;
 
 public interface BookService {
 
     List<BookLoanStatus> findAllBooks() throws SQLException;
 
+    // Admin
+    AddBookResponseDto addBook(AddBookRequestDto requestDto) throws SQLException;
 }
