150cb20e9e985c75c3c6ab203804abddc00c2f1a
heonuk <gitshineit@gmail.com>
2024-07-29 15:27:09 +0900

    :hammer: fix: 로그인 일반,관리자 구분을 객체 수정 및 응답 객체 수정

--- src/main/java/org/checkmate/common/dto/request/LoginRequestDto.java
+++ src/main/java/org/checkmate/common/dto/request/LoginRequestDto.java
@@ -1,6 +1,7 @@
 package org.checkmate.common.dto.request;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -11,6 +12,7 @@ import lombok.ToString;
  * HISTORY3: Lombok 적용                           [송헌욱  2024.07.25]
  */
 @Getter
+@Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
@@ -18,9 +20,5 @@ public class LoginRequestDto {
 
     private String loginId;  // 로그인 아이디(사원 번호)
     private String password; // 로그인 비밀번호
-
-    public static LoginRequestDto of(String loginId, String password) {
-        return new LoginRequestDto(loginId, password);
-    }
 
 }
