5730a10ed64852ef6d5e34e166cf8d7a11efa204
JUNI <junehee107@naver.com>
2024-07-29 17:41:21 +0900

    :recycle: refactor : 메서드 분리 리팩토링 진행

--- src/main/java/org/checkmate/admin/controller/view/BookUpdatePageController.java
+++ src/main/java/org/checkmate/admin/controller/view/BookUpdatePageController.java
@@ -5,11 +5,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
+import org.checkmate.admin.controller.server.BookController;
 import org.checkmate.admin.dto.request.BookUpdateRequestDto;
 import org.checkmate.admin.dto.response.BookReadInformationResponseDto;
 import org.checkmate.admin.service.BookManagementService;
@@ -17,6 +19,7 @@ import org.checkmate.admin.service.BookManagementServiceImpl;
 //import org.checkmate.common.controller.view.SceneManager;
 import org.checkmate.admin.dto.response.BookUpdateResponseDto;
 import org.checkmate.common.controller.view.SceneManager;
+import org.checkmate.common.exception.ValidationException;
 import org.checkmate.common.util.StringSplit;
 import org.checkmate.common.util.TypeFormatter;
 
@@ -32,56 +35,32 @@ import java.util.ResourceBundle;
 public class BookUpdatePageController implements Initializable {
 
     private Long bookId;
-    private final BookManagementService bookService;
+    private final BookController bookController;
 
     public BookUpdatePageController(Long bookId) {
-        bookService = new BookManagementServiceImpl();
+        bookController = new BookController();
         this.bookId = bookId;
     }
 
-    @FXML
-    private Label Menu;
-
-    @FXML
-    private Label MenuBack;
-
-    @FXML
-    private AnchorPane slider;
-
-    @FXML
-    private TextField bookTitle;
-
-    @FXML
-    private TextField publisher;
-
-    @FXML
-    private TextField isbn;
-
-    @FXML
-    private TextField author;
-
-    @FXML
-    private TextField translator;
-
-    @FXML
-    private ChoiceBox categories;
+    @FXML private Label Menu;
+    @FXML private Label MenuBack;
+    @FXML private AnchorPane slider;
+    @FXML private TextField bookTitle;
+    @FXML private TextField publisher;
+    @FXML private TextField isbn;
+    @FXML private TextField author;
+    @FXML private TextField translator;
+    @FXML private ChoiceBox categories;
 
     private boolean lStatus;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println(bookId);
         try {
-            BookReadInformationResponseDto responseDto = bookService.findBook(bookId);
-            bookTitle.setText(responseDto.getBName());
-            publisher.setText(responseDto.getPublisher());
-            isbn.setText(responseDto.getIsbn());
-            author.setText(responseDto.getAuthor());
-            translator.setText(responseDto.getTranslator());
-            String categoryValue = responseDto.getCategoryId()+". "+responseDto.getCName();
-            System.out.println(categoryValue);
-            categories.setValue(categoryValue);
-            lStatus = responseDto.getLStatus();
+            BookReadInformationResponseDto responseDto = bookController.readBook(bookId);
+            setData(responseDto);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -123,46 +102,40 @@ public class BookUpdatePageController implements Initializable {
 
     }
 
-    public boolean userField(TextField bookTitle,TextField isbn, TextField author, TextField translator, TextField publisher, ChoiceBox categories) {
-        if (bookTitle.getText().isEmpty()) {
-            Msg("책 제목을 입력해주세요.");
-            bookTitle.requestFocus();
-            bookTitle.clear();
-            return false;
-        }
-        if (isbn.getText().isEmpty()) {
-            Msg("책의 ISBN을 입력해주세요.");
-            isbn.requestFocus();
-            return false;
-        }
-        if (author.getText().isEmpty()) {
-            Msg("저자를 입력해주세요.");
-            author.requestFocus();
-            return false;
-        }
-        if (translator.getText().isEmpty()) {
-            Msg("옮긴이를 입력해주세요.");
-            author.requestFocus();
-            return false;
-        }
-        if (publisher.getText().isEmpty()) {
-            Msg("출판사를 입력해주세요.");
-            author.requestFocus();
-            return false;
-        }
-        // TODO : choice BOX 예외처리
-        return true;
+    private void setData(BookReadInformationResponseDto responseDto) throws SQLException{
+        bookTitle.setText(responseDto.getBName());
+        publisher.setText(responseDto.getPublisher());
+        isbn.setText(responseDto.getIsbn());
+        author.setText(responseDto.getAuthor());
+        translator.setText(responseDto.getTranslator());
+        String categoryValue = responseDto.getCategoryId()+". "+responseDto.getCName();
+        categories.setValue(categoryValue);
+        lStatus = responseDto.getLStatus();
     }
 
-    public void Msg(String msg) {
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    public void validateUserFields() {
+        isNotEmpty(bookTitle, "책 제목을 입력해주세요.");
+        isNotEmpty(isbn, "책의 ISBN을 입력해주세요.");
+        isNotEmpty(author, "저자를 입력해주세요.");
+        isNotEmpty(translator, "옮긴이를 입력해주세요.");
+        isNotEmpty(publisher, "출판사를 입력해주세요.");
+    }
+
+    private void isNotEmpty(TextField field, String message) {
+        if (field.getText().trim().isEmpty()) {
+            throw new ValidationException(message);
+        }
+    }
+
+    public void showAlert(String msg) {
+        Alert alert = new Alert(AlertType.WARNING);
         alert.setTitle("경고");
         alert.setHeaderText("도서 수정");
         alert.setContentText(msg);
         alert.show();
     }
 
-    public void Msg(String type,String msg) {
+    public void showAlert(String type,String msg) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(type);
         alert.setHeaderText("도서 수정");
@@ -170,29 +143,40 @@ public class BookUpdatePageController implements Initializable {
         alert.show();
     }
 
-    @FXML
-    public void register(ActionEvent actionEvent) throws NoSuchAlgorithmException, SQLException {
-        System.out.println("등록버튼실행됨");
-        if(!userField(bookTitle,isbn,author,translator,publisher,categories)){
-            return;
-        }
-
-        String bookTitle =  this.bookTitle.getText();
-        System.out.println(bookTitle);
+    private BookUpdateRequestDto createRequestDto(){
+        Long bookId = this.bookId;
+        String bookTitle = this.bookTitle.getText();
         String isbn = this.isbn.getText();
         String author = this.author.getText();
         String translator = this.translator.getText();
         String publisher = this.publisher.getText();
         int lStatusInt = TypeFormatter.BooleanToInteger(lStatus);
-        System.out.println(publisher);
         String category= (String) this.categories.getValue();
         int category_num = StringSplit.getCategoryNum(category,".");
-        System.out.println(category_num);
-        BookUpdateResponseDto responseDto = bookService.editBook(
-                BookUpdateRequestDto.of(bookId,bookTitle,isbn,author,translator,publisher,category_num,lStatusInt));
-        Msg("알림",responseDto.getMessage());
-        SceneManager sm = SceneManager.getInstance();
-        sm.moveScene("/org/checkmate/view/layouts/admin/bookManagementPage.fxml");
+
+        validateUserFields();
+
+        return BookUpdateRequestDto.builder()
+            .bookId(bookId)
+            .bookTitle(bookTitle)
+            .isbn(isbn)
+            .author(author)
+            .translator(translator)
+            .publisher(publisher)
+            .l_status(lStatusInt)
+            .category_num(category_num)
+            .build();
+    }
+
+    @FXML
+    public void register(ActionEvent actionEvent) throws NoSuchAlgorithmException, SQLException {
+        System.out.println("등록버튼실행됨");
+        try {
+            BookUpdateRequestDto requestDto = createRequestDto();
+            bookController.updateBook(createRequestDto());
+        }catch (ValidationException | SQLException e){
+            showAlert(e.getMessage());
+        }
     }
 
 }
