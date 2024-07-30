8a90626fc16661b0844feddd35e98a5705604d7f
heonuk <gitshineit@gmail.com>
2024-07-30 14:07:04 +0900

    :sparkles: feat: 나의 부서 도서 현황을 요청 로직 수행을 위한 서버 Controller 클래스 개발

--- src/main/java/org/checkmate/user/controller/server/BookController.java
+++ src/main/java/org/checkmate/user/controller/server/BookController.java
@@ -1,10 +1,9 @@
 package org.checkmate.user.controller.server;
 
-import org.checkmate.admin.dto.request.BookCreateRequestDto;
-import org.checkmate.admin.service.BookManagementService;
-import org.checkmate.admin.service.BookManagementServiceImpl;
 import org.checkmate.user.dto.request.CreateBookLoanRequestDto;
+import org.checkmate.user.dto.request.ReadMyDepartmentBookStatusRequestDto;
 import org.checkmate.user.dto.response.CreateBookLoanResponseDto;
+import org.checkmate.user.dto.response.ReadMyBookDeptStatusResDto;
 import org.checkmate.user.service.BookService;
 import org.checkmate.user.service.BookServiceImpl;
 
@@ -19,5 +18,20 @@ public class BookController {
 
     public CreateBookLoanResponseDto createLoanBook(CreateBookLoanRequestDto requestDto) throws SQLException {
         return bookService.createLoanBook(requestDto);
+    }
+
+    /**
+     * 팀 구성원 대여 현황
+     * @param loginId 사원 번호: View
+     * @param teamId 팀 식별자
+     * @return ReadMyBookDeptStatusResDto - 서비스 로직을 수행한 이후 요청에 대한 데이터 응답 객체
+     * @throws SQLException Database 예외
+     */
+    public ReadMyBookDeptStatusResDto readTeamMemberLendingStatus(String loginId, Long teamId) throws SQLException {
+        ReadMyDepartmentBookStatusRequestDto requestDto = ReadMyDepartmentBookStatusRequestDto.builder()
+                .loginId(loginId)
+                .teamId(teamId)
+                .build();
+        return bookService.findMyDepartmentStatus(requestDto);
     }
 }
