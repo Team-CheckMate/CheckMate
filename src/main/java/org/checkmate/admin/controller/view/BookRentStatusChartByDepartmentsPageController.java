73a726b02dd0d742e1f287f0cdd978d2bf00b6f1
JUNI <junehee107@naver.com>
2024-07-30 19:55:24 +0900

    :sparkles: feat : enum타입 departments구현하여 PIVOT으로 데이터 받아와 차트 데이터 주입

--- src/main/java/org/checkmate/admin/controller/view/BookRentStatusChartByDepartmentsPageController.java
+++ src/main/java/org/checkmate/admin/controller/view/BookRentStatusChartByDepartmentsPageController.java
@@ -1,58 +1,59 @@
 package org.checkmate.admin.controller.view;
 
 import com.jfoenix.controls.JFXButton;
+import java.sql.ResultSet;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
+import org.checkmate.admin.controller.server.BookController;
 import org.checkmate.admin.dto.response.ReadBookLoanRecordsForChartResponseDto;
+import org.checkmate.admin.mapper.BookManagementMapper;
 import org.checkmate.admin.service.BookManagementService;
 import org.checkmate.admin.service.BookManagementServiceImpl;
 
 import java.awt.*;
 import java.sql.SQLException;
+import org.checkmate.common.util.Departments;
 
 public class BookRentStatusChartByDepartmentsPageController {
 
-    private final BookManagementService bookService;
+    private final BookController bookController;
 
     // 기본 생성자
     public BookRentStatusChartByDepartmentsPageController() {
-        bookService = new BookManagementServiceImpl();
+        bookController = new BookController();
     }
 
 
     @FXML private BarChart<String, Number> barChart;
     @FXML private PieChart pieChart;
     private boolean isBarChartVisible = true;
-    ObservableList<ReadBookLoanRecordsForChartResponseDto> bookLoanRecordsForChartList;
 
     @FXML
     public void initialize() throws SQLException {
-        bookLoanRecordsForChartList = bookService.readDepartmentsBookLoanRecords();
-        // List<responseDto>를 ObservableList로 변환
-        ObservableList<XYChart.Data<String, Number>> chartData = FXCollections.observableArrayList();
-        for (ReadBookLoanRecordsForChartResponseDto record : bookLoanRecordsForChartList) {
-            chartData.add(new XYChart.Data<>(record.getName(), record.getCount()));
-        }
+        BookManagementMapper bm = new BookManagementMapper();
+        bm.readPivotDepartmentsBookLoanRecords();
 
-        // Series 생성 및 데이터 설정
         XYChart.Series<String, Number> series = new XYChart.Series<>();
         series.setName("대여한 책 개수");
-        series.setData(chartData);
-
-        // 시리즈 추가
+        for (Departments departments : Departments.values()) {
+            if (departments.getCount() > 0) {
+                series.getData().add(new XYChart.Data<>(departments.getDepartmentName(), departments.getCount()));
+            }
+        }
         barChart.getData().add(series);
 
-        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-        for (ReadBookLoanRecordsForChartResponseDto record : bookLoanRecordsForChartList) {
-            pieChartData.add(new PieChart.Data(record.getName(), record.getCount()));
+        for (Departments departments : Departments.values()) {
+            if (departments.getCount() > 0) {
+                pieChart.getData().add(new PieChart.Data(departments.getDepartmentName(), departments.getCount()));
+            }
         }
-        pieChart.setData(pieChartData);
-
     }
 
     @FXML
