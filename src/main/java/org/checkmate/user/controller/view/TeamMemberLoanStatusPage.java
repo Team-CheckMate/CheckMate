1794a929a39f03f8c75d73f713d5206111ac5021
heonuk <gitshineit@gmail.com>
2024-07-31 20:12:34 +0900

    :art: design: 나의 팀 현황 디자인

--- src/main/java/org/checkmate/user/controller/view/TeamMemberLoanStatusPage.java
+++ src/main/java/org/checkmate/user/controller/view/TeamMemberLoanStatusPage.java
@@ -29,36 +29,60 @@ import org.checkmate.user.dto.response.TeamMemberLoanStatusForView;
 public class TeamMemberLoanStatusPage implements Initializable {
 
     private final BookController server = new BookController();
+    private String eName;
+    private String tName;
+    private int totalLoanBook;
+    private int totalLastMonthLoanBook;
+    private int totalLastYearLoanBook;
 
-    @FXML private Hyperlink userNameLink;
-    @FXML private Text tdName;
-    @FXML private TableView<TeamMemberLoanStatusDegree> tableView;  // TableView 사용 데이터 타입
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column1; // 번호 (행 번호)
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, String> column2;  // 사원 번호 (loginId)
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, String> column3;  // 이름 (eName)
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column4; // 현재 대여 도서 수 (bookCount)
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column5; // 이번 달 읽은 도서 수 (currentMonthCount)
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column6; // 지난 달 읽은 도서 수 (lastMonthCount)
-    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column7; // 작년의 읽은 도서 수 (lastYearCount)
+    @FXML
+    private Hyperlink userNameLink;
+    @FXML
+    private Text tdName;
+    @FXML
+    private Text infoText;
+    @FXML
+    private TableView<TeamMemberLoanStatusDegree> tableView;  // TableView 사용 데이터 타입
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, Integer> column1; // 번호 (행 번호)
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, String> column2;  // 사원 번호 (loginId)
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, String> column3;  // 이름 (eName)
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, Integer> column4; // 현재 대여 도서 수 (bookCount)
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, Integer> column5; // 이번 달 읽은 도서 수 (currentMonthCount)
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, Integer> column6; // 지난 달 읽은 도서 수 (lastMonthCount)
+    @FXML
+    private TableColumn<TeamMemberLoanStatusDegree, Integer> column7; // 작년의 읽은 도서 수 (lastYearCount)
 
     @FXML
     private void exit(ActionEvent event) {
         Platform.exit();
     }
 
-    @FXML private void goToBookLoan(ActionEvent event) {
+    @FXML
+    private void goToBookLoan(ActionEvent event) {
         SceneManager sm = SceneManager.getInstance();
         sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml");
     }
-    @FXML private void goToLoanManage(ActionEvent event) {
+
+    @FXML
+    private void goToLoanManage(ActionEvent event) {
         SceneManager sm = SceneManager.getInstance();
         sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml"); //변경
     }
-    @FXML private void goToMyLoanBook(ActionEvent event) {
+
+    @FXML
+    private void goToMyLoanBook(ActionEvent event) {
         SceneManager sm = SceneManager.getInstance();
         sm.moveScene(READ_TM_LOAN_STATUS_FX.getFilePath());
     }
-    @FXML private void goToBookApply(ActionEvent event) {
+
+    @FXML
+    private void goToBookApply(ActionEvent event) {
         SceneManager sm = SceneManager.getInstance();
         sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml");
     }
@@ -68,8 +92,7 @@ public class TeamMemberLoanStatusPage implements Initializable {
         var session = LoginSession.getInstance();
         var userInfo = session.getUserInfo();
         userNameLink.setText(userInfo.getEName());
-        tdName.setText(userInfo.getDName()+" \n -> "+ userInfo.getTName());
-
+        tdName.setText(userInfo.getDName() + " \n -> " + userInfo.getTName());
 
         // 각 컬럼에 데이터 바인딩
         column2.setCellValueFactory(new PropertyValueFactory<>("loginId"));
@@ -82,7 +105,8 @@ public class TeamMemberLoanStatusPage implements Initializable {
         // 행 번호 설정
         column1.setCellFactory(new Callback<>() {
             @Override
-            public TableCell<TeamMemberLoanStatusDegree, Integer> call(TableColumn<TeamMemberLoanStatusDegree, Integer> param) {
+            public TableCell<TeamMemberLoanStatusDegree, Integer> call(
+                    TableColumn<TeamMemberLoanStatusDegree, Integer> param) {
                 return new TableCell<>() {
                     @Override
                     protected void updateItem(Integer item, boolean empty) {
@@ -102,19 +126,26 @@ public class TeamMemberLoanStatusPage implements Initializable {
     }
 
     private void loadData(String loginId, Long teamNo) {
-        System.out.println("11111111111");
         TeamMemberLoanStatusForView dataResponse = server.teamMemberLoanStatus(loginId, teamNo);
-        System.out.println("22222222222");
-        for (TeamMemberLoanStatusDegree degree : dataResponse.getList()) {
-            System.out.println(degree);;
-        }
-        System.out.println("3333333333");
+        int totalLoanBook = dataResponse.getTotalLoanBook(); // 총 빌린 도서 수
+        System.out.println("totalLoanBook = " + totalLoanBook);
+        int totalLastMonthLoanBook = dataResponse.getTotalLastMonthLoanBook();
+        System.out.println("totalLastMonthLoanBook = " + totalLastMonthLoanBook);
+        int totalLastYearBook = dataResponse.getTotalLastYearBook();
+        System.out.println("totalLastYearBook = " + totalLastYearBook);
         List<TeamMemberLoanStatusDegree> dataList = dataResponse.getList();
-
-        System.out.println("44444444444");
-        ObservableList<TeamMemberLoanStatusDegree> data = FXCollections.observableArrayList(dataList);
-        System.out.println(data);
-        System.out.println("55555555555");
+        ObservableList<TeamMemberLoanStatusDegree> data = FXCollections.observableArrayList(
+                dataList);
         tableView.setItems(data);
+    }
+
+    private String informationStringBuilder(String eName, String tName, int totalLoanBook,
+            int totalLastMonthLoanBook, int totalLastYearLoanBook) {
+        return eName
+                + "님이 속한 " + '\'' + tName + '\'' + " 팀은 현재 "
+                + totalLoanBook + "권을 대여 중이며," + '\n'
+                + "저번 달에는 " + totalLastMonthLoanBook + "권의 도서를 읽었습니다."
+                + '\n'
+                + "작년에는 " + totalLastYearLoanBook + "권의 도서를 읽으셨어요!";
     }
 }
