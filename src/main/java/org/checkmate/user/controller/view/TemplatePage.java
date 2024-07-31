e80772d392060c889a12d1f35dcffd34aa8ca4dd
heonuk <gitshineit@gmail.com>
2024-07-31 15:08:47 +0900

    :art: design: 디자인 템플릿 등록

--- src/main/java/org/checkmate/user/controller/view/TemplatePage.java
+++ src/main/java/org/checkmate/user/controller/view/TemplatePage.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
+import org.checkmate.common.controller.view.SceneManager;
 
 public class TemplatePage {
 
@@ -12,6 +13,26 @@ public class TemplatePage {
     @FXML
     private void exit(ActionEvent event) {
         Platform.exit();
+    }
+
+    @FXML private void goToBookLoan(ActionEvent event)
+    {
+        SceneManager sm = SceneManager.getInstance();
+        sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml");
+    }
+    @FXML private void goToLoanManage(ActionEvent event)
+    {
+        SceneManager sm = SceneManager.getInstance();
+        sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml"); //변경
+    }
+    @FXML private void goToMyLoanBook(ActionEvent event)
+    {
+        SceneManager sm = SceneManager.getInstance();
+        sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml");
+    }
+    @FXML private void goToBookApply(ActionEvent event)
+    {SceneManager sm = SceneManager.getInstance();
+        sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml");
     }
 
 }
