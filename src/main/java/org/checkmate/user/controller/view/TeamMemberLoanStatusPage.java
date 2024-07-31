d44d3c9480d2fd9bc553d144c31202145cbc9e9d
heonuk <gitshineit@gmail.com>
2024-08-01 02:09:04 +0900

    :art: design: 나의 팀 현황 디자인

--- src/main/java/org/checkmate/user/controller/view/TeamMemberLoanStatusPage.java
+++ src/main/java/org/checkmate/user/controller/view/TeamMemberLoanStatusPage.java
@@ -29,34 +29,20 @@ import org.checkmate.user.dto.response.TeamMemberLoanStatusForView;
 public class TeamMemberLoanStatusPage implements Initializable {
 
     private final BookController server = new BookController();
-    private String eName;
-    private String tName;
-    private int totalLoanBook;
-    private int totalLastMonthLoanBook;
-    private int totalLastYearLoanBook;
 
-    @FXML
-    private Hyperlink userNameLink;
-    @FXML
-    private Text tdName;
-    @FXML
-    private Text infoText;
-    @FXML
-    private TableView<TeamMemberLoanStatusDegree> tableView;  // TableView 사용 데이터 타입
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, Integer> column1; // 번호 (행 번호)
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, String> column2;  // 사원 번호 (loginId)
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, String> column3;  // 이름 (eName)
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, Integer> column4; // 현재 대여 도서 수 (bookCount)
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, Integer> column5; // 이번 달 읽은 도서 수 (currentMonthCount)
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, Integer> column6; // 지난 달 읽은 도서 수 (lastMonthCount)
-    @FXML
-    private TableColumn<TeamMemberLoanStatusDegree, Integer> column7; // 작년의 읽은 도서 수 (lastYearCount)
+    @FXML private Hyperlink userNameLink;
+    @FXML private Text tdName;
+    @FXML private Text deptNameText;
+    @FXML private Text infoText;
+    @FXML private Text anaMsg;
+    @FXML private TableView<TeamMemberLoanStatusDegree> tableView;  // TableView 사용 데이터 타입
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column1; // 번호 (행 번호)
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, String> column2;  // 사원 번호 (loginId)
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, String> column3;  // 이름 (eName)
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column4; // 현재 대여 도서 수 (bookCount)
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column5; // 이번 달 읽은 도서 수 (currentMonthCount)
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column6; // 지난 달 읽은 도서 수 (lastMonthCount)
+    @FXML private TableColumn<TeamMemberLoanStatusDegree, Integer> column7; // 작년의 읽은 도서 수 (lastYearCount)
 
     @FXML
     private void exit(ActionEvent event) {
@@ -72,7 +58,7 @@ public class TeamMemberLoanStatusPage implements Initializable {
     @FXML
     private void goToLoanManage(ActionEvent event) {
         SceneManager sm = SceneManager.getInstance();
-        sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml"); //변경
+        sm.moveScene("/org/checkmate/view/layouts/user/readLoanBookPage.fxml");
     }
 
     @FXML
@@ -89,10 +75,11 @@ public class TeamMemberLoanStatusPage implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        var session = LoginSession.getInstance();
+        LoginSession session = LoginSession.getInstance();
         var userInfo = session.getUserInfo();
         userNameLink.setText(userInfo.getEName());
-        tdName.setText(userInfo.getDName() + " \n -> " + userInfo.getTName());
+        deptNameText.setText(userInfo.getTName() + " 도서 대여 분석");
+        tdName.setText(userInfo.getDName() + "\n" + userInfo.getTName());
 
         // 각 컬럼에 데이터 바인딩
         column2.setCellValueFactory(new PropertyValueFactory<>("loginId"));
@@ -127,25 +114,55 @@ public class TeamMemberLoanStatusPage implements Initializable {
 
     private void loadData(String loginId, Long teamNo) {
         TeamMemberLoanStatusForView dataResponse = server.teamMemberLoanStatus(loginId, teamNo);
+
         int totalLoanBook = dataResponse.getTotalLoanBook(); // 총 빌린 도서 수
-        System.out.println("totalLoanBook = " + totalLoanBook);
-        int totalLastMonthLoanBook = dataResponse.getTotalLastMonthLoanBook();
-        System.out.println("totalLastMonthLoanBook = " + totalLastMonthLoanBook);
-        int totalLastYearBook = dataResponse.getTotalLastYearBook();
-        System.out.println("totalLastYearBook = " + totalLastYearBook);
+        int totalLastMonthLoanBook = dataResponse.getTotalLastMonthLoanBook(); // 지난달 대여 도서 수
+        int totalLastYearBook = dataResponse.getTotalLastYearBook(); // 작년 대여 도서 수
+
+        String[] analysis = readAnalysis(totalLoanBook, totalLastMonthLoanBook, totalLastYearBook);
+
         List<TeamMemberLoanStatusDegree> dataList = dataResponse.getList();
         ObservableList<TeamMemberLoanStatusDegree> data = FXCollections.observableArrayList(
                 dataList);
         tableView.setItems(data);
+        infoText.setText(analysis[0]);
+        anaMsg.setText(analysis[1]);
     }
 
-    private String informationStringBuilder(String eName, String tName, int totalLoanBook,
-            int totalLastMonthLoanBook, int totalLastYearLoanBook) {
-        return eName
-                + "님이 속한 " + '\'' + tName + '\'' + " 팀은 현재 "
-                + totalLoanBook + "권을 대여 중이며," + '\n'
-                + "저번 달에는 " + totalLastMonthLoanBook + "권의 도서를 읽었습니다."
-                + '\n'
-                + "작년에는 " + totalLastYearLoanBook + "권의 도서를 읽으셨어요!";
+    private String[] readAnalysis(int totalLoanBook, int totalLastMonthLoanBook, int totalLastYearBook) {
+        double monthChangePercent = calculateMonthChangePercent(totalLoanBook,
+                totalLastMonthLoanBook);
+        double yearAverageBooks = calculateYearAverageBooks(totalLastYearBook);
+
+        String info = String.format(
+                "총 대여 도서 수: %d권\n" +
+                "지난달 대여 도서 수: %d권\n" +
+                "작년 대여 도서 수: %d권",
+                totalLoanBook, totalLastMonthLoanBook, totalLastYearBook
+        );
+
+        String analysis = String.format(
+                "이번 달에는 지난달에 비해 %.2f%% %s했습니다.\n" +
+                "작년과 비교해보니 이대로라면 평균 %.2f권의 책을 읽을 수 있을 것 같습니다.",
+                Math.abs(monthChangePercent), monthChangePercent >= 0 ? "증가" : "감소",
+                yearAverageBooks
+        );
+
+        return new String[]{info, analysis};
     }
+
+    private double calculateMonthChangePercent(int totalLoanBook, int totalLastMonthLoanBook) {
+        if (totalLastMonthLoanBook != 0) {
+            return ((double) (totalLoanBook - totalLastMonthLoanBook) / totalLastMonthLoanBook) * 100;
+        }
+        return 0;
+    }
+
+    private double calculateYearAverageBooks(int totalLastYearBook) {
+        if (totalLastYearBook != 0) {
+            return (double) totalLastYearBook / 12;
+        }
+        return 0;
+    }
+
 }
