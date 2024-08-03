bdbac0659ce0c8aac2560dc176a9b4c59036712c
heonuk <gitshineit@gmail.com>
2024-08-03 15:04:39 +0900

    :recycle: refactor: 사용자 비밀번호 변경 Controller 분리

--- src/main/java/org/checkmate/user/controller/server/UserController.java
+++ src/main/java/org/checkmate/user/controller/server/UserController.java
@@ -1,5 +1,25 @@
 package org.checkmate.user.controller.server;
 
+import lombok.NoArgsConstructor;
+import org.checkmate.common.dto.response.CommonResponse;
+import org.checkmate.user.dto.request.ReqLoginIdAndCurPasswordAndUpdatePassword;
+import org.checkmate.user.dto.response.UpdatePasswordResponseDto;
+import org.checkmate.user.service.MemberService;
+import org.checkmate.user.service.MemberServiceImpl;
+
+@NoArgsConstructor
 public class UserController {
 
+    private final MemberService memberService = new MemberServiceImpl();
+
+    public CommonResponse<UpdatePasswordResponseDto> updatePassword(
+            String loginId, String curPassword,String updatePassword
+    ) {
+        ReqLoginIdAndCurPasswordAndUpdatePassword requestDto = ReqLoginIdAndCurPasswordAndUpdatePassword.builder()
+                .loginId(loginId)
+                .curPassword(curPassword)
+                .updatePassword(updatePassword)
+                .build();
+        return memberService.updatePassword(requestDto);
+    }
 }
